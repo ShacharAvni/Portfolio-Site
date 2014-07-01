1f885ddbe81620767f9265898b8e4965e89ed1d7
ShacharAvni <shac.avni@gmail.com>
2014-07-01 13:18:58 -0400

    Centered the email under the header and moved the "My Code" and "Resume" to the main navigation. Added e-mail obfuscation.

---
+++ scripts/contact.js
@@ -0,0 +1,31 @@
+//Copyright (c) 2014 Shachar Avni. All rights reserved.
+
+function getContact()
+{
+	document.write("<span class=\"contact\">");
+ 
+   document.write("<a class=\"headerOption\"");
+   document.write(" href=\"mai");
+   document.write("lto");
+   document.write(":");
+   document.write("&#115;&#104");
+   document.write("&#097;&#099;&#046;");
+   document.write("&#097;&#118;&#110;");
+   document.write("&#105;&#064;");
+   document.write("&#103;&#109;&#097;");
+   document.write("&#105;&#108;&#046;");
+   document.write("&#099;&#111;");
+   document.write("&#109;\">");
+
+   document.write("&#115;&#104&#097;");
+   document.write("&#099;&#046;");
+   document.write("&#097;&#118;");
+   document.write("&#110;&#105;");
+   document.write("&#064;&#103;");
+   document.write("&#109;&#097;&#105;");
+   document.write("&#108;&#046;");
+   document.write("&#099;&#111;");
+   document.write("&#109;</a>");
+ 
+   document.write("</span>");
+}
